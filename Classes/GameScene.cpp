18533407e22c86eb5bc44fdfb9b33e2b9c8ca95b
Valters Mednis <valters.mednis@gmail.com>
2015-08-22 15:15:03 +0300

    Add ground

--- Classes/GameScene.cpp
+++ Classes/GameScene.cpp
@@ -1,3 +1,4 @@
+#include <vector>
 #include "GameScene.h"
 
 USING_NS_CC;
@@ -6,6 +7,8 @@ cocos2d::Scene* GameScene::createScene()
 {
 	//Create and return scene with instance of this class attached as a child
 	auto scene = Scene::createWithPhysics();
+	//Enable physics debug
+	scene->getPhysicsWorld()->setDebugDrawMask(PhysicsWorld::DEBUGDRAW_ALL);
 	auto layer = GameScene::create();
 	scene->addChild(layer);
 	return scene;
@@ -19,7 +22,18 @@ bool GameScene::init()
 		return false;
 	}
 
-
+	//Ground
+	std::vector<Node *> groundNodes;
+	for (unsigned int i = 0; i < 10; i++)
+	{
+		auto groundPhysicsBody = PhysicsBody::createBox({ 128, 128 }, PhysicsMaterial(), { 64, 64 });
+		groundPhysicsBody->setDynamic(false);
+		auto node = Node::create();
+		node->setPhysicsBody(groundPhysicsBody);
+		node->setPosition(Vec2(i * 128, 0));
+		addChild(node);
+		groundNodes.push_back(node);
+	}
 
 	//Enable update function
 	scheduleUpdate();
